f9ff57a0ff4c490f5f6f4c1b10a60c7633440f28
daniil <daniilnazarov.262001@gmail.com>
2023-05-11 19:54:35 +0300

    _

--- project/src/chat/consumers.py
+++ project/src/chat/consumers.py
@@ -1,8 +1,142 @@
 # chat/consumers.py
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Room, Message
 from src.users.models import User
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from .serializers import MessageSerializer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+        self.user = None
+
+    def connect(self):
+        self.user = self.scope["user"]
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        self.room = Room.objects.filter(name=self.room_name).first()
+        #проверяем запрос
+        if not self.user.is_authenticated:
+            return   
+        if self.room == None:
+            return
+        if self.user not in self.room.participant.all():
+            return
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
+
+        self.accept()
+
+        #получаем 5 стартовых сообщений:
+        start_messages = Message.objects.filter(room = self.room).order_by('-time_message')[:5]
+        self.send(text_data=json.dumps(
+            {
+                "type": "start_messages",
+                "message": MessageSerializer(start_messages, many=True).data
+            }
+        ))
+
+
+
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message_type = text_data_json["type"]
+        if message_type == "chat_message":
+            message_text = text_data_json['message']
+            message = Message.objects.create(
+                user=self.user,
+                room=self.room,
+                text=message_text,
+            )
+            message.read_users.add(self.user)
+            message.save()
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    "type": "chat_message",
+                    "message": MessageSerializer(message).data,
+                },
+            )
+            
+
+    def chat_message(self, event):
+        self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': event['message']
+        }))
+
+
+
+'''
+class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+        self.user = None
+
+    def connect(self):
+        
+        self.user = self.scope["user"]
+        if not self.user.is_authenticated:
+            return
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        self.room = Room.objects.filter(name=self.room_name).first()
+        if str(self.room) == 'None':
+            return
+        #Join room group
+        self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+        self.send(text_data=json.dumps({
+            "type": "welcome_message",
+            "message": "Hey there! You've successfully connected!"
+        }))
+        
+    async def disconnect(self, close_code):
+        self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message_type = text_data_json["type"]
+        message = text_data_json['message']
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                }
+            )
+            print(message)
+
+    def chat_message(self, event):
+        message = event['message']
+        print('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -56,3 +190,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message
         }))
+        '''
+
+        
--- project/src/chat/serializers.py
+++ project/src/chat/serializers.py
@@ -2,19 +2,14 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from friendship.models import FriendshipRequest
 from .models import Room, Message
+from src.users.serializers import UserSerializer
 
+User = get_user_model()
 
-class RoomSerializer(serializers.ModelSerializer):
-    #chat_name = serializers.ReadOnlyField(source='get_name')
-    
+class RoomSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Room
         fields = ('id', 'display_name', 'name', 'participant', 'type')
-
-#class MessageSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = Message
-#        fields = ('id', 'user', 'room', 'text', 'read_users', 'time_message')
 
 class RoomSearchSerializer(serializers.Serializer):
     message = serializers.CharField(max_length = 100)
@@ -24,5 +19,14 @@ class RoomNameSerializer(serializers.Serializer):
     name = serializers.CharField(max_length = 128)
 
 
+class MessageSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    time_message = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+    class Meta:
+        model = Message
+        fields = ('user', 'text', 'time_message')
+
+    def get_user(self, obj):
+        return UserSerializer(obj.user).data
 
 
