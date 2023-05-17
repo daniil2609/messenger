8ba73cb459701142424a3af5d9084a96b84863ef
Nikita <mr.thekotik@gmail.com>
2023-05-17 20:20:45 +0300

    + уведомления

--- project/frontend/src/components/pages/Chats.js
+++ project/frontend/src/components/pages/Chats.js
@@ -6,6 +6,31 @@ import ListChats from "../ListChats"
 
 const Chats = () => { 
 
+    //вебсокет для уведомлений: 
+    const { readyStateNotifications } = useWebSocket("ws://127.0.0.1:8000/ws/chat/notifications/", { 
+        onOpen: () => { 
+            console.log("Connected Notifications!"); 
+        }, 
+        onClose: () => { 
+            console.log("Disconnected Notifications!"); 
+        }, 
+        // тут приходят сообщения с сервера 
+        onMessage: (e) => { 
+            const data = JSON.parse(e.data); 
+            switch (data.type) { 
+                case "notification": 
+                    console.log('Начало уведомления:') 
+                    console.log(data.message) 
+                    console.log(data.room_name) 
+                    console.log('Конец уведомления:') 
+                    break; 
+                default: 
+                    bash.error("Unknown message type!"); 
+                    break; 
+                } 
+        } 
+    });
+
     return(
         <>
         <HeaderPersonalPage/>
